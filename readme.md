4bcfe1b7fb02b99df825e9c04af69b3dd1178b77
rizwan <rizwan@rizwans-MBP.fios-router.home>
2022-12-27 16:04:29 -0500

    middleware and hot reload

---
+++ readme.md
@@ -0,0 +1,7 @@
+# A demo app for the ZIO Http
+
+### How to "hot reload" for development?
+1. I am using the `sbt-revolver` dependency in `project/plugins.sbt` for hot reloading.
+2. On terminal first type `sbt`
+3. While in the sbt prompt, type `~reStart` to run the in the hot reload mode.
+4. Or from intellij double click the `reStart` task from the `sbt tasks` in the sbt panel.
--- src/main/scala/ZIOHTTP.scala
+++ src/main/scala/ZIOHTTP.scala
@@ -3,6 +3,9 @@ import zio.*
 import zhttp.http.*
 import zhttp.service.Server
 
+import java.lang
+import scala.util.control
+
 object ZIOHTTP extends ZIOAppDefault:
 
   val port = 9000
@@ -20,10 +23,42 @@ object ZIOHTTP extends ZIOAppDefault:
   // <> (orElse) means if left fails then right will be tried.
   val combined = app ++ zApp
 
+  // middleware
+  val wrapped = combined @@ Middleware.debug
+  // request --> middleware --> combined
+
+  val loggingHttp = combined @@ Verbose.log
+
+
   val httpProgram = for {
     _ <- Console.printLine(s"Starting server at http://localhost:$port")
-    _ <- Server.start(port, combined)
+    _ <- Server.start(port, loggingHttp)
   } yield ()
 
   override def run: ZIO[Any, Any, Any] = httpProgram
 
+// request => contramap => http application => response => map => final response
+//         |-------------------- middleware --------------------|
+object Verbose {
+  def log[R, E >: Exception]:
+  Middleware[R, E, Request, Response, Request, Response] = new Middleware[R, E, Request, Response, Request, Response] {
+    override def apply[R1 <: R, E1 >: E](http: Http[R1, E1, Request, Response]): Http[R1, E1, Request, Response] =
+      http
+        .contramapZIO[R1, E1, Request](request =>
+          for {
+            _ <- Console.printLine(s"> ${request.method} ${request.path} ${request.version}")
+            _ <- ZIO.foreach(request.headers.toList) { header =>
+              Console.printLine(s"> ${header._1} ${header._2}")
+            }
+          } yield request
+        )
+        .mapZIO[R1, E1, Response](response =>
+          for {
+            _ <- Console.printLine(s"< ${response.status}")
+            _ <- ZIO.foreach(response.headers.toList) { header =>
+              Console.printLine(s"< ${header._1} ${header._2}")
+            }
+          } yield response
+      )
+  }
+}
